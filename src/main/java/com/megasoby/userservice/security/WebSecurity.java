1e0e0ea0bf67cfdf2fed588d335fda4d4acab181
megasoby <megasoby@ssg.com>
2022-01-31 13:06:47 +0900

    user-service add module

--- src/main/java/com/megasoby/userservice/security/WebSecurity.java
+++ src/main/java/com/megasoby/userservice/security/WebSecurity.java
@@ -1,19 +1,54 @@
 package com.megasoby.userservice.security;
 
+import com.megasoby.userservice.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
 
-    @Override
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override // 권한
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
-
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/users/**")
+                .hasIpAddress("192.168.219.105")
+                .and()
+                .addFilter(getAuthenticationFilter());
+        
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter =
+                new AuthenticationFilter(authenticationManager(), userService, env);
+//        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    // select pwd from users where email =?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override //인증
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- src/main/java/com/megasoby/userservice/service/UserService.java
+++ src/main/java/com/megasoby/userservice/service/UserService.java
@@ -2,11 +2,14 @@ package com.megasoby.userservice.service;
 
 import com.megasoby.userservice.dto.UserDto;
 import com.megasoby.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String userName);
 }
